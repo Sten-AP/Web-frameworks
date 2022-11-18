1d55a1026904a5fd1932bd2ae0738252ca042dbc
Sten Hulsbergen <stenhulsbergen@gmail.com>
2022-11-19 00:29:05 +0100

    pokedex httpclient

---
+++ Angular/Pokedex_HttpClientModule/src/app/pokemon/pokemon.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { PokedexService } from "../pokedex.service";
+
+@Component({
+ selector: "app-pokemon",
+ templateUrl: "./pokemon.component.html",
+ styleUrls: ["./pokemon.component.css"],
+})
+export class PokemonComponent implements OnInit {
+ service: PokedexService;
+
+ constructor(service: PokedexService) {
+  this.service = service;
+ }
+
+ get pokemons() {
+  return this.service.pokemons;
+ }
+
+ get favorited() {
+  return this.service.favorited;
+ }
+
+ changeState(id: string) {
+  this.favorited[Number(id) - 1] = !this.favorited[Number(id) - 1];
+ }
+
+ getPokemonById(id: string) {
+  this.service.selected = Number(id) - 1;
+ }
+
+ ngOnInit(): void {}
+}
