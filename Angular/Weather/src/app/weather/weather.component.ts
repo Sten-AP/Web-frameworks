ba5462fdece5d03994ce54b4178bc5708cf2555d
Sten Hulsbergen <stenhulsbergen@gmail.com>
2022-11-22 10:49:13 +0100

    weather klaar

--- Angular/Weather/src/app/weather/weather.component.ts
+++ Angular/Weather/src/app/weather/weather.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { WeatherDataService, IWeatherData } from "../weather-data.service";
-
 @Component({
   selector: "app-weather",
   templateUrl: "./weather.component.html",
@@ -8,21 +7,17 @@ import { WeatherDataService, IWeatherData } from "../weather-data.service";
 })
 export class WeatherComponent implements OnInit {
   data: any;
+  imgUrl: string;
+
   constructor(private service: WeatherDataService) {}
 
   ngOnInit(): void {}
 
   getGemeente(gemeente: string) {
     this.service._url = gemeente;
-    this.getWeatherData();
-  }
-
-  getWeatherData() {
-    console.log(this.service.url);
-
     this.service.weatherData.subscribe((data) => {
       this.data = data;
-      console.log(this.data);
+      this.imgUrl = `https://openweathermap.org/img/w/0${Math.floor(this.data.main.temp - 273.15) % 4}d.png`;
     });
   }
 }
